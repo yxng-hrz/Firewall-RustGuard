8d55d65267093310082cb68daca06ed9ddfe3ea7
Guillaume Houriez <guillaume.houriez@epitech.eu>
2025-04-16 10:22:31 +0200

    [ADD] ajout d'un structure pricipale pour la config de l'application, avec l'appel des règles, et la blacklist

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,6 @@ use std::path::Path;
 use anyhow::{Result, Context};
 
 // Structure principale de la configuration de l'application
-// Contient les paramètres généraux, les règles du pare-feu et la configuration de la liste noire
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct AppConfig {
     pub general: GeneralConfig,
